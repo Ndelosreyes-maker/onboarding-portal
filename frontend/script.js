56db015ef4aeecf837ef4828a79be28998cc4d5a
Ndelosreyes-maker <Ndelosreyes@legendarytherapy.com>
2026-02-25 02:22:01 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,14 @@
 
+document.getElementById("uploadForm").addEventListener("submit", async e=>{
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const res = await fetch("http://localhost:3000/upload",{
+    method:"POST",
+    body:formData
+  });
+
+  const data = await res.text();
+  document.getElementById("msg").innerText = data;
+});
